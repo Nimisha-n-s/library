d3dada482bf430e489e32d784d7aa5e5e7ce8114
Nimisha-n-s <nimishasukulal@gmail.com>
2023-05-25 15:55:26 +0530

    library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Navigation from './components/Navigation';
+import { Route, Routes } from 'react-router-dom';
+
+import Login from './components/Login';
+import Home from './components/Home';
+import Signup from './components/Signup';
+
+import AdminLogin from './components/AdminLogin';
+import Homepage from './components/Homepage';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    <Navigation />
+    <Routes> 
+      <Route path="/" element={<Home />} />
+      <Route path="/l" element={< Login/>} />
+    
+     <Route path='/s' element={<Signup/>} />
+     
+     <Route path='/a' element={<AdminLogin/>} />
+     <Route path='/hp' element={<Homepage/>} />
+    </Routes>
     </div>
   );
 }
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,14 @@
+import { TextField, Typography } from '@mui/material'
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>
+        <br/>
+        <br/> <br/> <br/> <br/>     
+      <Typography>hello</Typography>
+    </div>  
+  )
+}
+
+export default Homepage
